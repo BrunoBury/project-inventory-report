6ad891ad301ed1f20e18234c2e48d55e20f9485a
Bruno <bruno_bury@hotmail.com>
2024-01-30 12:02:13 -0300

    create test_product_report, req02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,22 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="123",
+        product_name="test product",
+        company_name="test company",
+        manufacturing_date="01/01/2000",
+        expiration_date="01/01/2001",
+        serial_number="123456",
+        storage_instructions="test instructions",
+    )
+
+    expected_report = (
+        "The product 123 - test product with serial number 123456 "
+        "manufactured on 01/01/2000 by the company test company "
+        "valid until 01/01/2001 must be stored according to the "
+        "following instructions: test instructions."
+    )
+
+    assert str(product) == expected_report
